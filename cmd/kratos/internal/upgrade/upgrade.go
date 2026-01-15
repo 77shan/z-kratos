59ab1c448ca773820e409f576a04e43d2d95fef8
hui.lin <hui.lin@zettlab.com>
2026-01-15 11:46:44 +0800

    feat: go-kratos module update

--- cmd/kratos/internal/upgrade/upgrade.go
+++ cmd/kratos/internal/upgrade/upgrade.go
@@ -21,7 +21,7 @@ func Run(_ *cobra.Command, _ []string) {
 	err := base.GoInstall(
 		"github.com/go-kratos/kratos/cmd/kratos/v2@latest",
 		"github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2@latest",
-		"github.com/go-kratos/kratos/cmd/protoc-gen-go-errors/v2@latest",
+		"github.com/77shan/go-gen-errors/cmd/protoc-gen-go-errors/v2@latest",
 		"google.golang.org/protobuf/cmd/protoc-gen-go@latest",
 		"google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest",
 		"github.com/google/gnostic/cmd/protoc-gen-openapi@latest",
